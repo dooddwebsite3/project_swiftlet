a70591309101f0d01eb5a8d6fc1eda469c427a66
dooddwebsite3 <dooddwebsite3@gmail.com>
2018-10-13 15:14:47 +0700

    จับแยก

---
+++ eBusiness/include/footer.php
@@ -0,0 +1,159 @@
+ 
+
+
+
+
+  <!-- Start Footer bottom Area -->
+  <footer>
+    <div class="footer-area">
+      <div class="container">
+
+
+
+    <div class="row">
+    <div class="col-sm-2 "> 
+    <div class="footer-logo">
+                  <h2><span></span>Swiftlet</h2>
+                </div>
+    </div>
+    <div class="col-sm-6"> <p>Swiftlet Co., Ltd. (Headquarter)
+                              120/14 Moo 2 Ngamwongwan Road, Bangkhen,
+                              Muang Nonthaburi, Nonthaburi 11000 THAILAND 
+                              <br>Tel: +662-102-1946
+                              <br>contact@swiftlet.co.th
+    </div>
+    <div class="col-sm-4"> 
+       <div class="footer-icons">
+                  <ul>
+                    <li>
+                      <a href="#"><i class="fa fa-facebook"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-twitter"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-google"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-pinterest"></i></a>
+                    </li>
+                  </ul>
+        </div>
+    </div>
+    </div>
+
+        <div class="row">
+          <div class="col-md-12 col-sm-4 col-xs-12">
+            <div class="footer-content">
+              <div class="footer-head">
+
+                <div class="col-sm-2"> 
+                About Swiftlet 
+                <ul>
+                  <li>Manifesto</li>
+                  <li>Our stories</li>
+                  <li>Our Contribution</li>
+                </ul> 
+                </div>
+
+                <div class="col-sm-2"> 
+                Terebinth 
+                <ul>
+                  <li> Why ERP</li>
+                  <li> What is Terebinth</li>
+                  <li> Solutions</li>
+                </ul>
+                </div> 
+                <div class="col-sm-2">
+                IT Services
+                <ul>
+                  <li>Capabilities</li>
+                  <li>Clients</li>
+                </ul>  
+                </div>
+                <div class="col-sm-2"> 
+                Learn with Swiftlet 
+                <ul>
+                  <li>Ruby on Rail</li>
+                  <li>Xxxx</li>
+                  <li>Xxxx</li>
+                  <li>Xxxx</li>
+                </ul>
+                </div>  
+                <div class="col-sm-2"> 
+                Career 
+                <ul>
+                    <li>Life at Swiftlet</li>
+                    <li>Internship</li>
+                    <li>Developer</li>
+                    <li>Software QA</li>
+                    <li>DevOps Engineer</li>
+                    <li>UI Designer</li>
+                    <li>UX Designer</li>
+                    <li>Project Manager</li>
+                </ul>
+                
+        
+                </div>
+                <div class="col-sm-2"> 
+                News 
+                </div>                 
+              </div>
+            </div>
+          </div>
+          <!-- end single footer -->
+      
+        </div>
+      </div>
+    </div>
+    
+  </footer>
+
+
+  
+
+
+
+
+
+
+
+
+
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+
+
+
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/venobox/venobox.min.js"></script>
+  <script src="lib/knob/jquery.knob.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/parallax/parallax.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/nivo-slider/js/jquery.nivo.slider.js" type="text/javascript"></script>
+  <script src="lib/appear/jquery.appear.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <!-- Uncomment below if you want to use dynamic Google Maps -->
+  <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8HeI8o-c1NppZA-92oYlXakhDPYR7XMY"></script> -->
+
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <script src="js/main.js"></script>
+
+
+
+ 
+
+
+
+</body>
+
+</html>
+
